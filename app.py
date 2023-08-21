bcb9e1c576d680d207111236a6626069adb5fa09
sahirf <130094961+sahirf@users.noreply.github.com>
2023-08-21 10:34:26 -0500

    Update4 app.py

--- app.py
+++ app.py
@@ -2,7 +2,9 @@ from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
-ip_list = []
+def save_ip_to_file(ip_address):
+    with open('ip_addresses.txt', 'a') as file:
+        file.write(ip_address + '\n')
 
 @app.route('/')
 def index():
@@ -12,11 +14,13 @@ def index():
 def record_ip():
     data = request.get_json()
     ip_address = data.get('ip')
-    ip_list.append(ip_address)
+    save_ip_to_file(ip_address)
     return jsonify({"message": "IP recorded successfully."})
 
 @app.route('/api/get_ips', methods=['GET'])
 def get_ips():
+    with open('ip_addresses.txt', 'r') as file:
+        ip_list = file.read().splitlines()
     return jsonify({"ip_list": ip_list})
 
 if __name__ == '__main__':
