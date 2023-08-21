534fe98cb01cb64079cc6a29f7464c812d25bb49
sahirf <130094961+sahirf@users.noreply.github.com>
2023-08-21 10:14:08 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,20 @@
-from flask import Flask, request, jsonify
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
 ip_list = []
 
-@app.route('/api/record_ip', methods=['GET', 'POST'])
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/api/record_ip', methods=['POST'])
 def record_ip():
-    ip_address = request.remote_addr
+    data = request.get_json()
+    ip_address = data.get('ip')
     ip_list.append(ip_address)
     return jsonify({"message": "IP recorded successfully."})
 
-@app.route('/api/get_ips', methods=['GET'])
-def get_ips():
-    return jsonify({"ip_list": ip_list})
-
 if __name__ == '__main__':
     app.run(debug=True)
+
